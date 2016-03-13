f16b6d0accb39fd19919d37389b0193c592427a3
volodink <volodin.konstantin@gmail.com>
2016-03-13 16:47:53 +0300

    Добавлен пример шаблона на базе CMake.

---
+++ cmake-template/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "add.h"
+
+/**
+\author your_user_here
+\brief this is main function, cap.
+*/
+int main(void) {
+    printf("Hello, world %d!!!\n", add(1,2));
+    return 0;
+}
